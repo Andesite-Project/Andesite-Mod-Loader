b88dcf48be7464954657718651987bf5c2c52b45
Marius Lindvall <marius@varden.info>
2015-06-03 22:42:33 +0200

    Implemented item stack translator

--- src/main/java/info/varden/andesite/modloader/ForgeItemStackTranslator.java
+++ src/main/java/info/varden/andesite/modloader/ForgeItemStackTranslator.java
@@ -6,7 +6,11 @@
 package info.varden.andesite.modloader;
 
 import info.varden.andesite.core.wrapper.AndesiteItemStack;
+import net.minecraft.block.Block;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraftforge.fml.common.registry.GameData;
+import net.minecraftforge.fml.common.registry.GameRegistry;
 
 /**
  *
@@ -14,10 +18,17 @@ import net.minecraft.item.ItemStack;
  */
 public abstract class ForgeItemStackTranslator {
     public static AndesiteItemStack translate(ItemStack stack) {
-        return null;
+        return new AndesiteItemStack(GameRegistry.findUniqueIdentifierFor(stack.getItem()).toString(), stack.stackSize, stack.getMetadata());
     }
     
     public static ItemStack translate(AndesiteItemStack stack) {
-        return null;
+        if (GameData.getItemRegistry().containsKey(stack.getID())) {
+            return new ItemStack(Item.getByNameOrId(stack.getID()), stack.getAmount(), stack.getMeta());
+        } else if (GameData.getBlockRegistry().containsKey(stack.getID())) {
+            return new ItemStack(Block.getBlockFromName(stack.getID()), stack.getAmount(), stack.getMeta());
+        } else {
+            AndesiteML.error("The item name " + stack.getID() + " is not present as an item or block on this Minecraft installation!");
+            return new ItemStack(Block.getBlockFromName("minecraft:air"), 0);
+        }
     }
 }
